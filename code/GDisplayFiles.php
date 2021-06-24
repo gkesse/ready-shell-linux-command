3f39c3e3d4e8a1ffe78c05094b3ece241f894d71
Gerard KESSE <tiakagerard@hotmail.com>
2021-06-24 09:58:44 +0200

    Initial Commit

--- code/GDisplayFiles.php
+++ code/GDisplayFiles.php
@@ -8,6 +8,11 @@ class GDisplayFiles extends GWidget {
     //===============================================
     public function run() {
         echo sprintf("<h1>GDisplayFiles</h1>\n");
+        echo sprintf("<div><div class='border2'>\n");
+        for($i = 0; $i < 100; $i++) {
+            echo sprintf("<div class='border3'>Image</div>\n");
+        }
+        echo sprintf("</div></div>\n");
     }
     //===============================================
 }
